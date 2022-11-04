cf44b4a7ee86a87d952e675d735e7fb5c298ed86
Allan Jardine <allan@datatables.net>
2022-11-04 14:51:27 +0000

    Missing files in DataTables-DataTables

--- js/buttons.semanticui.js
+++ js/buttons.semanticui.js
@@ -1,6 +1,51 @@
+
 /*! Bootstrap integration for DataTables' Buttons
  * ©2016 SpryMedia Ltd - datatables.net/license
  */
+
+(function( factory ){
+	if ( typeof define === 'function' && define.amd ) {
+		// AMD
+		define( ['jquery', 'datatables.net-se', 'datatables.net-buttons'], function ( $ ) {
+			return factory( $, window, document );
+		} );
+	}
+	else if ( typeof exports === 'object' ) {
+		// CommonJS
+		module.exports = function (root, $) {
+			if ( ! root ) {
+				// CommonJS environments without a window global must pass a
+				// root. This will give an error otherwise
+				root = window;
+			}
+
+			if ( ! $ ) {
+				$ = typeof window !== 'undefined' ? // jQuery's factory checks for a global window
+					require('jquery') :
+					require('jquery')( root );
+			}
+
+			if ( ! $.fn.dataTable ) {
+				require('datatables.net-se')(root, $);
+			}
+
+			if ( ! $.fn.dataTable ) {
+				require('datatables.net-buttons')(root, $);
+			}
+
+
+			return factory( $, root, root.document );
+		};
+	}
+	else {
+		// Browser
+		factory( jQuery, window, document );
+	}
+}(function( $, window, document, undefined ) {
+'use strict';
+var DataTable = $.fn.dataTable;
+
+
 
 $.extend( true, DataTable.Buttons.defaults, {
 	dom: {
@@ -47,3 +92,7 @@ $(document).on('buttons-popover.dt', function () {
 		$('.dtsp-panesContainer').removeClass('vertical buttons')
 	}
 });
+
+
+return DataTable;
+}));
